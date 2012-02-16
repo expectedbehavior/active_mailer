b7944cc1c0814d13c6f3d5685bd82659000b30dd
Jason Gladish <jason@expectedbehavior.com>
2012-02-16 16:59:49 -0500

    Test scaffold that actually runs

--- test/active_mailer_test.rb
+++ test/active_mailer_test.rb
@@ -1,8 +1,7 @@
-require 'test/unit'
+require 'test_helper'
 
-class ActiveMailerTest < Test::Unit::TestCase
-  # Replace this with your real tests.
-  def test_this_plugin
-    flunk
+class ActiveMailerTest < ActiveSupport::TestCase
+  test "truth" do
+    assert_kind_of Module, ActiveMailer
   end
 end
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '8d79cfa565bdd53d567f5636ef05b805e64faa59cba043ce3a00df12942d6d2a217415cae2c782aef71c77e32b0404f9a3730538c3327e06a1715640ca754a9b'
