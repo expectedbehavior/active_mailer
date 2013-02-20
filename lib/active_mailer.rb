e60edc0bfc62df591c573925585901118e0bd782
Matthew Wade Gordon, esq <matt@expectedbehavior.com>
2013-02-20 14:28:22 -0500

    This makes it way more convenient to create tables with the correct basic columns.

--- lib/active_mailer.rb
+++ lib/active_mailer.rb
@@ -11,3 +11,16 @@ module ActiveMailer
 end
 
 require "active_mailer/engine"
+
+ActiveRecord::ConnectionAdapters::SchemaStatements.module_eval do
+  def create_active_mailer_table(*args, &block)
+    block_with_default_columns = Proc.new do |t|
+      t.integer   :sender_id
+      t.timestamp :sent_at
+      t.string    :subject
+      block.call(t)
+    end
+    
+    create_table(*args, &block_with_default_columns)
+  end  
+end 
