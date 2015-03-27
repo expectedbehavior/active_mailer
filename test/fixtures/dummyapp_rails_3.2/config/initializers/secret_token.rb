42c26b22dd44d625e895a24e9cf884c7215a28c4
Elijah Miller <elijah.miller@gmail.com>
2015-03-27 13:30:05 -0400

    Remove Rails 3.2 app.

--- test/fixtures/dummyapp_rails_3.2/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-DummyappRails32::Application.config.secret_token = '67185739e1a149121d9ab14638a1e3baa546c2891b331ed3532d6af645243089a7923f1c0d10109decef8cc937c21d554fbabbc77fbfd598d7ddd4265afce425'
--- test/fixtures/dummyapp_rails_3.2/db/migrate/20130220200227_create_invitation_emails.rb
+++
@@ -1,8 +0,0 @@
-class CreateInvitationEmails < ActiveRecord::Migration
-  def change
-    create_active_mailer_table :invitation_emails do |t|
-      t.integer :invitation_id
-      t.timestamps
-    end
-  end
-end
