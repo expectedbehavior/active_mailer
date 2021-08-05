125a836e8f2e6f4698cbe39142af59c58a2d91ad
Matthew Gordon <matthew.w.gordon@gmail.com>
2021-08-05 17:10:02 -0400

    Update dummy application to be Rails 5/6 compatible
    Why is this change needed?
    --------------------------
    We want ActiveMailer to be Rails 6 compatible, so the dummy app for testing needs to support those versions.
    
    How does it address the issue?
    ------------------------------
    I include some updates from Thoughtbot's version in Clearance while maintaining the specific changes needed for this repo. It does not work as-is because Rails 5 expects to find a manifest at app/assets/config/manifest.js and it's not finding it on load even after I create it. Some more debugging will need to happen.
    
    Any links to any relevant tickets, articles or other resources?
    ---------------------------------------------------------------

--- test/fixtures/dummy/application.rb
+++ test/fixtures/dummy/application.rb
@@ -16,7 +16,6 @@ module Dummy
     config.action_mailer.delivery_method = :test
     config.active_support.deprecation = :stderr
     config.active_support.test_order = :random
-    config.assets.enabled = true
     config.cache_classes = true
     config.consider_all_requests_local = true
     config.eager_load = false
@@ -24,15 +23,25 @@ module Dummy
     config.paths["app/controllers"] << "#{APP_ROOT}/app/controllers"
     config.paths["app/models"] << "#{APP_ROOT}/app/models"
     config.paths["app/views"] << "#{APP_ROOT}/app/views"
+    config.paths["app/assets"] << "#{APP_ROOT}/app/assets"
     config.paths["config/database"] = "#{APP_ROOT}/config/database.yml"
     config.paths["db"] = "#{APP_ROOT}/db"
     config.paths["db/migrate"] = "#{APP_ROOT}/db/migrate"
     config.paths["log"] = "tmp/log/development.log"
-    config.secret_token = "SECRET_TOKEN_IS_MIN_30_CHARS_LONG"
+    config.paths.add "config/routes.rb", with: "#{APP_ROOT}/config/routes.rb"
+    config.secret_key_base = "SECRET_KEY_BASE"
 
-    if Rails::VERSION::MAJOR >= 4
-      config.secret_key_base = "SECRET_KEY_BASE"
+    if config.active_record.sqlite3.respond_to?(:represent_boolean_as_integer)
+      if Rails::VERSION::MAJOR < 6
+        config.active_record.sqlite3.represent_boolean_as_integer = true
+      end
     end
+
+    if Rails::VERSION::MAJOR >= 6
+      config.action_mailer.delivery_job = "ActionMailer::MailDeliveryJob"
+    end
+
+    config.active_job.queue_adapter = :inline
 
     def require_environment!
       initialize!
@@ -44,3 +53,48 @@ module Dummy
     end
   end
 end
+
+
+# require "rails/all"
+
+# module Dummy
+#   APP_ROOT = File.expand_path("..", __FILE__).freeze
+
+#   I18n.enforce_available_locales = true
+
+#   class Application < Rails::Application
+#     config.action_controller.allow_forgery_protection = false
+#     config.action_controller.perform_caching = false
+#     config.action_dispatch.show_exceptions = false
+#     config.action_mailer.default_url_options = { host: "dummy.example.com" }
+#     config.action_mailer.delivery_method = :test
+#     config.active_support.deprecation = :stderr
+#     config.active_support.test_order = :random
+#     config.assets.enabled = true
+#     config.cache_classes = true
+#     config.consider_all_requests_local = true
+#     config.eager_load = false
+#     config.encoding = "utf-8"
+#     config.paths["app/controllers"] << "#{APP_ROOT}/app/controllers"
+#     config.paths["app/models"] << "#{APP_ROOT}/app/models"
+#     config.paths["app/views"] << "#{APP_ROOT}/app/views"
+#     config.paths["config/database"] = "#{APP_ROOT}/config/database.yml"
+#     config.paths["db"] = "#{APP_ROOT}/db"
+#     config.paths["db/migrate"] = "#{APP_ROOT}/db/migrate"
+#     config.paths["log"] = "tmp/log/development.log"
+#     config.secret_token = "SECRET_TOKEN_IS_MIN_30_CHARS_LONG"
+
+#     if Rails::VERSION::MAJOR >= 4
+#       config.secret_key_base = "SECRET_KEY_BASE"
+#     end
+
+#     def require_environment!
+#       initialize!
+#     end
+
+#     def initialize!(&block)
+#       FileUtils.mkdir_p(Rails.root.join("db").to_s)
+#       super unless @initialized
+#     end
+#   end
+# end
