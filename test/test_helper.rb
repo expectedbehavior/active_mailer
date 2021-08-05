09f1158257ac01fe89713d5ba505149005f7acda
Matthew Gordon <matthew.w.gordon@gmail.com>
2021-08-05 17:07:54 -0400

    Determine which version Appraisal is testing before including the dummy application
    Why is this change needed?
    --------------------------
    On the path to improving our testing, we may want to include a different Rails application depending on what version of Rails we're trying to test. To do that, we'll need to know what gemfile Appraisal loaded.
    
    How does it address the issue?
    ------------------------------
    The ENV contains the name of the gemfile. By convention in this project, that contains the Rails version under test. I'm parsing it out in the worst way possible. I expect there to be another pass at this before this code rests.
    
    Any links to any relevant tickets, articles or other resources?
    ---------------------------------------------------------------

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,6 +6,14 @@ require "rails/all"
 $: << File.join(File.dirname(__FILE__), "lib")
 require "active_mailer"
 
+
+# This is a very dumb way of getting the current Rails version
+# appraisal is running out of the name of the bundle gemfile. If you
+# start using a different naming scheme for the gemfiles, this will
+# fall apart.
+appraisal_version = ENV['BUNDLE_GEMFILE'].split("/").last.split("rails").last.split(".gemfile")
+
+# require File.expand_path("../fixtures/rails-#{appraisal_version}/application.rb",  __FILE__)
 require File.expand_path("../fixtures/dummy/application.rb",  __FILE__)
 require "rails/test_help"
 
